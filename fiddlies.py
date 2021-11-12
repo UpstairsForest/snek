bf58aaed8d4a9db54d50fdaedad6e087a50d5641
UpstairForest <nikita.batalov@uzh.ch>
2021-11-12 19:27:59 +0100

    prettier graphics

--- fiddlies.py
+++ fiddlies.py
@@ -1,12 +1,20 @@
 # RGB values
 background_colour = (0, 0, 0)
-snake_colour = (100, 255, 100)
+snake_colour = (150, 255, 70)
+snake_border_colour = (50, 75, 50)
 apple_colour = (255, 100, 50)
+apple_border_colour = (75, 30, 30)
 
 # sizes
 dis_x = 900
-dis_y = 900
-step = 45
+dis_y = 800
+step = 50
+border_width = 8
 
 # game speed
 ticks_per_frame = 200
+
+# checks:
+if (dis_x%step, dis_y%step) != (0, 0):
+    print("ugly grid:step ratio")
+
--- main.py
+++ main.py
@@ -20,12 +20,16 @@ def reposition_apple(passed_snek):
 dis_x = fiddlies.dis_x
 dis_y = fiddlies.dis_y
 step = fiddlies.step
+bw = fiddlies.border_width
 head = step * ((np.array([int(dis_x/2), int(dis_y/2)])) // step)
 print(head/step)
 
 pygame.init()
 dis = pygame.display.set_mode((dis_x, dis_y))
-pygame.draw.rect(dis, fiddlies.snake_colour, [head[0], head[1], step, step])
+
+pygame.draw.rect(dis, fiddlies.snake_colour, [head[0]+bw, head[1]+bw, step-2*bw, step-2*bw])
+pygame.draw.rect(dis, fiddlies.snake_border_colour, [head[0], head[1], step, step],
+                             width=bw, border_radius=5)
 pygame.display.update()
 pygame.display.set_caption("Kukik")
 clock = pygame.time.Clock()
@@ -75,9 +79,13 @@ while not game_over:
             snek = np.append(np.array([head]), snek[:-1], axis=0)
 
         dis.fill(fiddlies.background_colour)
-        pygame.draw.rect(dis, fiddlies.apple_colour, [apple[0], apple[1], step, step])
+        pygame.draw.rect(dis, fiddlies.apple_colour, [apple[0]+bw, apple[1]+bw, step-2*bw, step-2*bw])
+        pygame.draw.rect(dis, fiddlies.apple_border_colour, [apple[0], apple[1], step, step],
+                         width=bw, border_radius=5)
         for chunk in snek:
-            pygame.draw.rect(dis, fiddlies.snake_colour, [chunk[0], chunk[1], step, step])
+            pygame.draw.rect(dis, fiddlies.snake_colour, [chunk[0]+bw, chunk[1]+bw, step-2*bw, step-2*bw])
+            pygame.draw.rect(dis, fiddlies.snake_border_colour, [chunk[0], chunk[1], step, step],
+                             width=bw, border_radius=5)
         pygame.display.update()
         t = 0
 
